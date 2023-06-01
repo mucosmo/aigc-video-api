3bbecd181b15430de4d91f18d697971edc3a3093
huanyu.yang <mfuture@qq.com>
2023-06-01 15:53:19 +0800

    initial commit

---
+++ src/config/config.default.ts
@@ -0,0 +1,9 @@
+import { MidwayConfig } from '@midwayjs/core';
+
+export default {
+  // use for cookie sign key, should change to your own and keep security
+  keys: '1685605603807_6903',
+  koa: {
+    port: 7001,
+  },
+} as MidwayConfig;
